3644038fef85b346b34e28a7c9358324c38ea8f4
Gullrye <wmwspecter@163.com>
2023-02-12 18:23:02 +0800

    feat: vue 和 nvue 分别加载 iconfont

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
-import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+import { onLaunch, onShow, onHide } from '@dcloudio/uni-app'
 onLaunch(() => {
-  console.log("App Launch");
-});
+  console.log('App Launch')
+  // nvue 使用基于 Weex 的原生渲染，引入图标库方式如下
+  // #ifdef APP-NVUE
+  const domModule = uni.requireNativePlugin('dom')
+  domModule.addRule('fontFace', {
+    fontFamily: 'iconfont',
+    src: "url('http://at.alicdn.com/t/font_1859985_7mxozsfdvib.ttf')"
+  })
+  // #endif
+})
 onShow(() => {
-  console.log("App Show");
-});
+  console.log('App Show')
+})
 onHide(() => {
-  console.log("App Hide");
-});
+  console.log('App Hide')
+})
 </script>
-<style></style>
+
+<style>
+@import './common/free.css';
+@import './common/common.css';
+/* nvue 时，iconfont 只能用 unicode */
+/* #ifndef APP-PLUS-NVUE */
+@import './common/icon.css';
+/* #endif */
+</style>
